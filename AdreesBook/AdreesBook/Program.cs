7f95bc02664dfb3ad68472bbefb363d55fb44427
tushar <tusharshitre123@gmail.com>
2022-01-31 20:03:55 +0530

    Contact creation

--- AdreesBook/AdreesBook/Program.cs
+++ AdreesBook/AdreesBook/Program.cs
@@ -10,8 +10,39 @@ namespace AdreesBook
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("welcome to the address book");
+            //Showing Welcome Message On console.
+            Console.WriteLine("Welcome to Address Book Progam \n");
+
+            Console.Write("Enter First Name, Last Name, Address, City, State, Zip, Phone Number, Email \n");
+            CreateAddressBook addressBook = new CreateAddressBook()  //Initializing elements using collection-initializer syntax
+            {
+                FirstName = Console.ReadLine(),
+                LastName = Console.ReadLine(),
+                Address = Console.ReadLine(),
+                City = Console.ReadLine(),
+                State = Console.ReadLine(),
+                Zip = Convert.ToDouble(Console.ReadLine()),
+                PhoneNumber = Convert.ToDouble(Console.ReadLine()),
+                Email = Console.ReadLine(),
+            };
+            addressBook.displayContact();
             Console.ReadLine();
         }
+
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
